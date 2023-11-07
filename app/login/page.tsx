1771d3b73e34e740ad3b23fd3bd1d0118c966fd0
Bryan Grados <bryangrados9@gmail.com>
2023-11-06 22:50:41 -0500

    Add Next UI, Framer Motion, and Next Themes dependencies, update Supabase dependencies, and refactor login page and layout components

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,12 @@
-const LoginPage = () => {
-	return <div>LoginPage</div>;
+import { createClient } from "@/src/utils/supabase/server";
+import { Button } from "@nextui-org/react";
+import { cookies } from "next/headers";
+
+const LoginPage = async () => {
+	const cookieStore = cookies();
+	const supabase = createClient(cookieStore);
+
+	return <Button>LoginPage</Button>;
 };
 
 export default LoginPage;
